bfd8586f2c19801c8140d6e981821ff71f5d71c2
Cristián Maureira-Fredes <cristian.maureira-fredes@qt.io>
2022-11-24 12:05:53 +0100

    Add handle for tikzpicture
    closes #6

--- patterns.py
+++ patterns.py
@@ -14,6 +14,7 @@ def get_pattern_scope(v):
         "align",
         "displaymath",
         "gather",
+        "tikzpicture",
     ]
     patterns = [fr"\\{v}{{ *{w}\** *}}" for w in words]
     if v == "begin":
